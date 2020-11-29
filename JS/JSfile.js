74ef3b1b2398d65425dbc52bf74a3016497bd3f0
vop-bg <75174819+vop-bg@users.noreply.github.com>
2020-11-29 21:45:32 +0000

    Update JSfile.js

--- JS/JSfile.js
+++ JS/JSfile.js
@@ -1,3 +1,9 @@
+var request = new XMLHttpRequest();
+   request.open("GET", "data/file.json", false);
+   request.send(null)
+   var my_JSON_object = JSON.parse(request.responseText);
+   alert (my_JSON_object.result[0]);
+
 
 let url = 'https://raw.githubusercontent.com/mishelmishev/DentalManager/main/PriceListVOP.json';
 function allPrices(){
